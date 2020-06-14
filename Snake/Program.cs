cc988060e0dd312754fcd7ebc537bac894e06c5c
Илья Рощупкин <roshupkin.ilya2012@yandex.ru>
2020-06-14 18:05:29 +0300

    Lists

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -12,7 +12,21 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(10, 10,'&');
-            p1.Draw();
+            Point p2 = new Point(10, 20, '&');
+            Point p3 = new Point(20, 10, '&');
+            Point p4 = new Point(10, 5, '&');
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            List<char> symList = new List<char>();
+            symList.Add('@');
+            symList.Add('#');
+            symList.Add('$');
+            symList.Add('%');
             Console.ReadLine();
         }
     }
