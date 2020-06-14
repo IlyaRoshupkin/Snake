c01f4eaa1754822385beceb19a940aecdb2adc48
Илья Рощупкин <roshupkin.ilya2012@yandex.ru>
2020-06-14 17:28:42 +0300

    Functions

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -11,8 +11,17 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 10;
+            int y1 = 10;
+            char sym1 = '*';
+            Draw(x1, y1, sym1);
             Console.ReadLine();
+        }
+
+        private static void Draw(int x1, int y1, char sym1)
+        {
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sym1);
         }
     }
 }
